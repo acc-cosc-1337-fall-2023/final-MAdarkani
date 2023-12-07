d5c0a097e3457d560c2c8843606ae10ca823892e
Milad Adarkani <143145275+MAdarkani@users.noreply.github.com>
2023-12-07 00:42:11 +0000

    Question 2

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,18 @@
 //
+#include "roll.h"
+
+Roll::Roll(Die& d1, Die& d2) : die1(d1), die2(d2), rolled(false), value(0) {}
+
+void Roll::roll_die()
+{
+    die1.roll();
+    die2.roll();
+    value = die1.rolled_value() + die2.rolled_value();
+
+    rolled = true;
+}
+
+int Roll::roll_value() const
+{
+    return value;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -17,5 +18,23 @@ TEST_CASE("Die rolls return a value from 1 to 6") {
 		
 		REQUIRE(rolledValue >= 1);
 		REQUIRE(rolledValue <= 6);
+	}
+}
+
+
+TEST_CASE("testing die roll with Roll class") {
+	Die die1;
+	Die die2;
+
+	Roll roll(die1, die2);
+
+	for (int i=0; i<10; i++)
+	{
+		roll.roll_die();
+		
+		int rolledValue = roll.roll_value();
+		
+		REQUIRE(rolledValue >= 2);
+		REQUIRE(rolledValue <= 12);
 	}
 }
